64ce3b79cb0e3b09389a38a144ccc7e8d0954c46
ConiMJG <constanzaj4@gmail.com>
2023-09-13 17:44:12 -0300

    ACTUALIZACIÓN 12

---
+++ JS/tipodedatos.js
@@ -0,0 +1,58 @@
+let add = 29; 
+let sueldo = 120000; 
+const PI = 3.14;
+
+const n_grande = 1e6; 
+const n_pequeno = 1e-6; 
+
+console.log(n_grande)
+console.log(n_pequeno)
+
+
+const bigInt = 123456789123456789123456789123456789;
+console.log(bigInt)
+
+console.log(edad / 0);
+
+console.log("2"/2);
+
+console.log("Hola" / 2);
+console.log("Hola" * 4);
+
+
+console.log('########################STRINGS#########################')
+let nombre = "Constanza"
+let ciudad = "Osorno"
+let capital = "Puerto Montt"
+let string1 = "Hola ¿Cómo estás?";
+let string2 = '¡Buenas Tardes!';
+let frase = 'Estte es un saludo: ${string1}';
+
+console.log(ciudad + "y" + capital + "pertenecen a la Región de Los Lagos")
+console.log(capital, "es la capital de la Región de Los Lagos")
+console.log("Mi nombre es " .concat(nombrre, "y vivo en ", ciudad))
+
+console.log('############## BOOLEANS  ########' )
+let V = true;
+let F = false; 
+
+
+if (V) {
+    console.log("Es verdadero"); 
+
+} else {
+    console.log("Es falso");
+}
+
+
+console.log('#########NULL#############')
+let numero_nuevo = null
+console.log(numero_nuevo)
+
+console.log('########### UNDEFINED ################')
+let institución; 
+console.log(institución)
+
+
+console.log('############ OBJETOS ##########')
+let user = ne
---
+++ JS/variables.js
@@ -0,0 +1,22 @@
+var estatura = 1.71 
+let peso = 60 
+
+
+const nombre = "Constanza"
+
+var edad
+let ciudad
+
+edad = 21
+Ciudad = "Osorno"
+
+const apellido = "Jaramillo"
+
+const nombrecompleto = nombre + " " + apellido
+console.log(nombrecompleto);
+
+
+let docente = 'Tomas', age = 25, asignaturas = ['IA', 'Seguridad Informática'];
+
+console.log(n);
+var n = 10; 
