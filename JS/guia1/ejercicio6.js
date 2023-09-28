e05f579a6b4d37def207a1c3e00f61fdb64b14c1
ConiMJG <104464328+ConiMJG@users.noreply.github.com>
2023-09-28 09:21:21 -0400

    Update ejercicio6.js

--- JS/guia1/ejercicio6.js
+++ JS/guia1/ejercicio6.js
@@ -6,19 +6,30 @@
 //es ese producto y que el stock es crítico. Si todos los productos tienen un stock igual o
 //superior a 1000 unidades, el programa mostrará un mensaje que indique que el inventario
 //está en buen estado.
-///////////////////////////////////////////////////////////////////////////////////////////////////////
 
 let inventario=[];//Crearemos un arreglo para almacenar los productos
-
 
 //Ahora una función para representar cada producto que se pueda ingresar.      
 function productos(){                   
     let product={};
 
-    product.nombre=prompt('Nombre: ');
-    product.precio=prompt('Precio: ');
-    product.stock=prompt('cantidad: ');
+    product.nombre=prompt('Ingrese el nombre del producto: ');
+    while(product.nombre.trim() === '' || !/^[a-zA-Z\s]+$/.test(product.nombre)){
+      alert("El nombre no es válido");
+      product.nombre=prompt('Ingrese el nombre del producto: ');
+    }
 
+    product.precio=parseFloat(prompt('Ingrese el precio del producto: '));
+    while (isNaN(product.precio) || product.precio <=0) {
+        alert("El precio no es válido");
+        product.precio=parseFloat(prompt("Ingrese el precio del producto: "));
+    }
+
+    product.stock=parseInt(prompt('Ingrese el stock del producto: '));
+    while (isNaN(product.stock) || product.stock <=0) {
+      alert("El stock no es válido");
+      product.stock=parseInt(prompt("Ingrese el stock del producto: "));
+    }
     inventario.push(product);
 }
 
@@ -27,8 +38,8 @@ const ingreso = prompt('¿Cuántos productos deseas ingresar?: ', 1);
 
 for (let i = 0; i < ingreso; i++) {
   productos();
+  alert(`El producto ha sido ingresado correctamente`)
 }
-
 
 //luego de haberlos ingresados, recorreremos el arreglo donde se encuentra los productos para saber
 //si existe alguno en estado crítico
@@ -40,13 +51,24 @@ for (let i=0; i<inventario.length;i++){
     let cont=0
     if (cant<1000){
         critic.push(nombre);
-        //alert(`el stock de ${nombre} es crítico`); *OPCIONAL*
         cont+=1
     }
+
 }
+
 let critico=critic.join(', ');
 if (cont=0){
     alert('El inventario se encuentra en buen estado con los productos con buen stock.');
 }else{
     alert(`Los productos en estádo crítico (Stock menor a 1000 unidades) son: ${critico}`);
+}
+
+alert('Tupla final del inventario es -->');
+
+for (let i = 0; i < inventario.length; i++) {
+  let producto = inventario[i];
+  alert(`***PRODUCTO ${i+1}***\n
+        Nombre: ${producto.nombre}\n
+         Precio: ${producto.precio}\n
+         Stock: ${producto.stock}\n`);
 }
