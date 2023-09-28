f1b846e6e35bad51bd5744272329c62427323e3f
ConiMJG <104464328+ConiMJG@users.noreply.github.com>
2023-09-28 09:20:35 -0400

    Update ejercicio4.js

--- JS/guia1/ejercicio4.js
+++ JS/guia1/ejercicio4.js
@@ -1,16 +1,20 @@
 //Solicitar al usuario que ingrese una serie de números separados por comas. Encontrar y
 //mostrar el número más grande entre los números ingresados
 
-const num = prompt("Ingrese una serie de números separados por comas: ", '');
+let numEntrada = prompt("Ingrese una serie de números separados por comas: ", '');
+while (numEntrada.trim() === '') {
+    alert("Por favor, ingresa números válidos.");
+    numEntrada = prompt('Ingrese una serie de números separados por comas: ', '');
+  }
 
-const numCadenas = num.split(',');
+const numCadenas = numEntrada.split(',');
 
-let numGrande = parseInt(numCadenas[0]);;
+let numGrande = parseFloat(numCadenas[0]);;
 
-for (let i = 0; i < numCadenas.length; i++) {
-    const numero = parseInt(numCadenas[i]);
-    if (numero > numGrande) {
+for (let i = 1; i < numCadenas.length; i++) {
+    let numero = parseFloat(numCadenas[i]);
+    if (!isNaN(numero) && numero > numGrande) {
         numGrande = numero;
     }
 }
-alert("El número más grande es: " + numGrande);
+alert(`El número más grande es: ${numGrande}`);
